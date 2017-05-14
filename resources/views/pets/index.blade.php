f820eaade55583d750d3bfec748065d88be37fa0
Seb <sebrao@live.co.uk>
2017-05-14 22:37:35 +0100

    Changed a few styles

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -5,27 +5,30 @@
         <h1 class="page-header">Checkout our top doggies!</h1>
         @foreach($pets as $pet)
             <div class="panel" style="padding: 15px">
-                <div class="media">
-                    <div class="media-left media-top">
-                        <a href="#">
-                            <img class="media-object" src="http://lorempixel.com/100/100/animals/8/" alt="...">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">{{ $pet->breed }}</h3>
-                        <p>
-                            {{ $pet->created_at }}
-                        </p>
-                        <p>
-                            {{ $pet->description }}
-                        </p>
-                        <p>
-                            {{ $pet->user->name }}
-                        </p>
-                    </div>
-                </div>
-            </div>
 
+                    <div class="media">
+                        <div class="media-left media-top">
+                            <a href="/pets/{{ $pet->id }}">
+                                <img class="media-object" src="http://lorempixel.com/100/100/animals/8/" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <a href="/pets/{{ $pet->id }}">
+                                <h3 class="media-heading">{{ $pet->breed }}</h3>
+                            </a>
+                            <p>
+                                {{ $pet->created_at }}
+                            </p>
+                            <p>
+                                {{ $pet->description }}
+                            </p>
+                            <p>
+                                {{ $pet->user->name }}
+                            </p>
+                        </div>
+                    </div>
+
+            </div>
         @endforeach
 
         {{ $pets->links() }}
