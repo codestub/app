bb988444ac9ef255fd53a404c3a5d11e39ed3b1a
Seb <sebrao@live.co.uk>
2017-05-14 17:25:42 +0100

    Changed a bit of bootstrap

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -6,16 +6,21 @@
         @foreach($pets as $pet)
             <div class="panel" style="padding: 15px">
                 <div class="media">
-                    <div class="media-left media-middle">
+                    <div class="media-left media-top">
                         <a href="#">
                             <img class="media-object" src="http://lorempixel.com/100/100/animals/8/" alt="...">
                         </a>
                     </div>
                     <div class="media-body">
                         <h3 class="media-heading">{{ $pet->breed }}</h3>
-                        {{ $pet->created_at }}
+                        <p>
+                            {{ $pet->created_at }}
+                        </p>
                         <p>
                             {{ $pet->description }}
+                        </p>
+                        <p>
+                            {{ $pet->user->name }}
                         </p>
                     </div>
                 </div>
